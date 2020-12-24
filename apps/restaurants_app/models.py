f3d43389632a6bdea429b7d9b50f645f8c73fb10
Jd2020D <jed19of@gmail.com>
2020-12-24 14:19:59 +0200

    fixed dashboard page

--- apps/restaurants_app/models.py
+++ apps/restaurants_app/models.py
@@ -11,12 +11,13 @@ class Restaurant(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    iconLink=models.TextField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Meal(models.Model):
     title = models.CharField(max_length=255)
-    imgLink=models.CharField(max_length=255)
+    imgLink=models.TextField()
     price=models.DecimalField(decimal_places=2, max_digits=5)
     description=models.TextField()
     category=models.ForeignKey(Category, related_name="meals", on_delete = models.CASCADE)
