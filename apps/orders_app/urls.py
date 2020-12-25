29fd5e689183795ae7349467b4c51d4ee4e17a2f
Jd2020D <jed19of@gmail.com>
2020-12-25 19:24:32 +0200

    'update'

--- apps/orders_app/urls.py
+++ apps/orders_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.mealsPage),
+    path('add_to_cart',views.addMealToCart,name='addToCart')
 
 
 ]
--- apps/orders_app/views.py
+++ apps/orders_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render,redirect
+from django.http import JsonResponse
+
 from . import models
 from django.contrib import messages
 
 
 def mealsPage(request):
-    if 'id' not in request.session:
-                return redirect('/')
-    if 'action' in request.session:
-            del request.session['action']
+    # if 'id' not in request.session or 'partner_id' in request.session :
+    #             return redirect('/')
     if 'values' in request.session:
         del request.session['values']
-    # return render(request,'quotes_page.html',context)
+    return render(request,'meals.html')
     #pass
+def addMealToCart(request):
+    return JsonResponse({'add':1})
