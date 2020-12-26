0e4fd76f101876a9a1355013ed3eafda5ca3b8a1
xkhateebx <khateebo.new@gmail.com>
2020-12-26 11:01:20 +0200

    'recived'

--- apps/restaurants_app/models.py
+++ apps/restaurants_app/models.py
@@ -5,7 +5,7 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     phoneNumber = models.CharField(max_length=255)
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -19,9 +19,28 @@ class Meal(models.Model):
     title = models.CharField(max_length=255)
     imgLink=models.TextField()
     price=models.DecimalField(decimal_places=2, max_digits=5)
-    description=models.TextField()
+    description=models.CharField(max_length=97)
     category=models.ForeignKey(Category, related_name="meals", on_delete = models.CASCADE)
     restaurant=models.ForeignKey(Restaurant, related_name="meals", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 #aaaaa
+def getRestaurantById(id):
+    try:
+        return Restaurant.objects.get(id=id)
+    except:
+        return False
+
+def checkCat(name):
+    try:
+        Category.objects.get(name=name)
+        return True
+    except:
+        return False
+
+
+def creatMeal(data,restu_id):
+    category=Category.objects.get(name=data['category'])
+    restaurant=getRestaurantById(restu_id)
+    meal=Meal.objects.create(title=data['title'],imgLink=data['imgLink'],price=data['price'],description=data['description'],category=category,restaurant=restaurant)
+    return meal.id
--- apps/restaurants_app/views.py
+++ apps/restaurants_app/views.py
@@ -1,14 +1,46 @@
+from django.contrib.messages.api import error
 from django.shortcuts import render,redirect
 from . import models
 from django.contrib import messages
 
-
-def partnerPage(request):
-    if 'id' not in request.session:
-                return redirect('/')
+def clearTempSessions(request):
     if 'action' in request.session:
             del request.session['action']
     if 'values' in request.session:
         del request.session['values']
-    # return render(request,'quotes_page.html',context)
+
+def partnerPage(request):
+    # if 'id' not in request.session:
+    #             return redirect('/signin')
+    # if 'partner_id' not in request.session:
+    #             return redirect('/signup/partner')
+    clearTempSessions(request)
+    return render(request,'dashboard.html')
     #pass
+def addMeal_valditor(data):
+    errors={}
+    try:
+        if len(data['title'])<2:
+            errors['title_length'] ='title less than 2'
+        if not (data['imgLink'].endswith('jpg') or data['imgLink'].endswith('png')):
+            errors['imgLink'] ='please upload valid extintion'
+    except:
+        errors['invalid input']='dont change inputs'
+    return errors
+def addMeal(request):
+    # if 'id' not in request.session:
+    #             return redirect('/signin')
+    # if 'partner_id' not in request.session:
+    #             return redirect('/signup/partner')
+
+    if request.method=='POST':
+        errors=addMeal_valditor(request.POST)
+        if len(errors) > 0:
+            for value in errors.values():
+                messages.error(request, value)
+            request.session['values']=request.POST
+        else:
+            id=models.createMeal(request.POST,request.session.partner_id)
+            return redirect('/partner')       
+    return redirect('/')
+
